48856168665c9673d0baed2e8d7989cd81f6f61e
sravan <sravan-s@protonmail.com>
2023-09-17 20:28:30 +0900

    bugfix: B-Jn to A-Jn

--- 2023_09_17.hs
+++ 2023_09_17.hs
@@ -1,6 +1,8 @@
+-- London to heathrow
+-- We dont do error handling
+-- We assume that we have a valid input
 import System.IO
 import Control.Monad
--- London to heathrow
 data Lane = A | B deriving (Show, Eq)
 
 data Section = Section {
@@ -15,28 +17,26 @@ data Path = Path {
   , lane :: Lane
 } deriving (Show)
 
--- We dont do error handling
 toSections :: [Int] -> [Section]
 toSections [x] = []
 toSections (x:y:z:xs) = Section { divider = x, toA = y, toB = z }: toSections xs
 
+prependZero :: [Int] -> [Int]
+prependZero xs = 0:xs
 
+-- We assume 0 is distance between  A0 and B0 -> ie- > [30, 90, ...] -> [0, 30, 90, ...]
 parseInput :: String -> [Section]
-parseInput =  toSections . (0:) . fmap read . lines
-  -- We assume 0 is distance between  A0 and B0 -> ie- > [30, 90, ...] -> [0, 30, 90, ...]
-  --
+parseInput =  toSections . prependZero . fmap read . lines
 
--- [Path is a stack for ease]
 predicate :: Path -> Section -> Path
 predicate Path{ distance = 0 } section 
   | toA section > toB section = Path{ distance = toB section, lane = B, secNo = 0 }
   | otherwise = Path{ distance = toA section, lane = A, secNo = 0 }
-
 predicate p section
   | prevLane == A && toAVal <= toBVal + dividerVal = Path{ distance = prevDistance + toAVal, lane = A, secNo = prevSecNo + 1 }
   | prevLane == A && toAVal > toBVal + dividerVal  = Path{ distance = prevDistance + toBVal + dividerVal, lane = B, secNo = prevSecNo + 1 }
   | prevLane == B && toBVal <= toAVal + dividerVal = Path{ distance = prevDistance + toBVal, lane = B, secNo = prevSecNo + 1 }
-  | prevLane == B && toBVal > toAVal + dividerVal  = Path{ distance = prevDistance + toAVal + dividerVal, lane = B, secNo = prevSecNo + 1 }
+  | prevLane == B && toBVal > toAVal + dividerVal  = Path{ distance = prevDistance + toAVal + dividerVal, lane = A, secNo = prevSecNo + 1 }
   where 
     prevDistance = distance p
     prevSecNo = secNo p
@@ -53,7 +53,6 @@ main :: IO ()
 main = do
   handle <- openFile "london_to_heathrow.txt" ReadMode
   contents <- hGetContents handle
-  let output = compute (parseInput contents)
-  -- we can print path from output list :)
+  let output = compute $ parseInput contents
+  -- we can pretty-print path from output list :)
   print output
-
